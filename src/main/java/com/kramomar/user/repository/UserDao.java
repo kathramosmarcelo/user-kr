54dd9f721b3e5f0843315d7e7a8e4cb36eaf9fa8
KathRamosMarcelo <i201811907@cibertec.edu.pe>
2022-02-24 19:20:27 -0500

    redis implementado en user

--- src/main/java/com/kramomar/user/repository/UserDao.java
+++ src/main/java/com/kramomar/user/repository/UserDao.java
@@ -10,13 +10,16 @@ import com.kramomar.user.entity.User;
 @Repository
 public class UserDao {
 
+	
 	public static final String HASH_KEY = "User";
 	private RedisTemplate template;
+	
 	
 	public User save(User user) {
     template.opsForHash().put(HASH_KEY, user.getId(), user);
 	return user;
 	}
+	
 	
 	public List<User> findAllUsers(){
 		return template.opsForHash().values(HASH_KEY);
